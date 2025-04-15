5dd0d8930e7f7d06532125c32fefa4da05f2acab
Gamershmidt <gamershmidt.sofya@bk.ru>
2025-04-15 04:20:38 +0300

    search upd

--- app/search.sh
+++ app/search.sh
@@ -4,10 +4,8 @@ echo "This script will include commands to search for documents given the query 
 
 source .venv/bin/activate
 
-# Python of the driver (/app/.venv/bin/python)
 export PYSPARK_DRIVER_PYTHON=$(which python)
 
-# Python of the excutor (./.venv/bin/python)
 export PYSPARK_PYTHON=./.venv/bin/python
 
 QUERY="$1"
@@ -18,7 +16,6 @@ if [ -z "$QUERY" ]; then
   exit 1
 fi
 
-# Run the query with user input as an argument
 spark-submit \
   --master yarn \
   --packages com.datastax.spark:spark-cassandra-connector_2.12:3.4.1 \
